e20d02ec4c82ea4ed1c06a8a956d09f3383fb39f
Jônatas Lima de Medeiros <jonatas.lima.medeiros@live.com>
2024-07-15 19:05:13 -0300

    Winget script added

---
+++ Codigos/LogicaDeProgramacao/dadosPessoa.py
@@ -0,0 +1,7 @@
+nome: str = input("Nome: ")
+idade: int = int(input("Idade: "))
+salario: float = float(input("Salário: "))
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Salário: {salario}")
---
+++ Codigos/LogicaDeProgramacao/somaDoisNumeros.py
@@ -0,0 +1,7 @@
+num1 = int(input("Primeiro número: "))
+num2 = int(input("Segundo número: "))
+
+print(f"Soma = {num1 + num2}")
+print(f"Subtração = {num1 - num2}")
+print(f"Multiplicação = {num1 * num2}")
+print(f"Divisão = {num1 / num2}")
