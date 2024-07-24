ce31eb92448c70fd3be55f17f30ec7390619b299
Jônatas Lima de Medeiros <jonatas.lima.medeiros@live.com>
2024-07-24 18:15:11 -0300

    feat: Script de instalação para Windows atualizado

---
+++ scripts/winget.ps1
@@ -0,0 +1,6 @@
+winget install -e --id Oracle.JDK.17
+winget install -e --id OpenJS.NodeJS
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Git.Git
+
+Set-ExecutionPolicy RemoteSigned
