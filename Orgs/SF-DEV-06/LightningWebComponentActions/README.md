abc67fa21cd689324c4d6b986f0b9cc8f9e5b1d9
Jônatas Lima de Medeiros <jonatas.lima.medeiros@live.com>
2024-07-24 23:42:46 -0300

    feat: Adicionada pasta de orgs com subpastas para cada turma

---
+++ Orgs/SF-DEV-06/LightningWebComponentActions/README.md
@@ -0,0 +1,6 @@
+# QualificaSP - Turma Dev-06
+
+Trilhas e módulos que utilizamos essa org:
+
+1. https://trailhead.salesforce.com/content/learn/modules/lex_javascript_button_migration?trailmix_creator_id=qualifica-sp&trailmix_slug=desenvolvedor-salesforce-qualifica-sp
+2. https://trailhead.salesforce.com/content/learn/projects/set-up-your-lightning-web-components-developer-tools?trailmix_creator_id=qualifica-sp&trailmix_slug=desenvolvedor-salesforce-qualifica-sp
---
+++ Orgs/SF-DEV-06/LightningWebComponentActions/force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -0,0 +1,21 @@
+import { LightningElement, track } from 'lwc';
+export default class MyFirstWebComponent extends LightningElement {
+    @track
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Amy Taylor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+    ];
+}
