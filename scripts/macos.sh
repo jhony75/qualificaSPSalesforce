c8c2c2ed76891cf55416f226fb2dab99ed74c4ca
Jônatas Lima de Medeiros <jonatas.lima.medeiros@live.com>
2024-08-03 16:17:03 -0300

    feat(scripts): Installation script updated

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -2,6 +2,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install openjdk
 brew install node
+brew install git
 brew install --cask visual-studio-code
 brew update
 
